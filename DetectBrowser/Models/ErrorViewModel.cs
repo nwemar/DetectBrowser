2a1d39e7aeedd9c30ca883d2fd732aaafe25c522
nwemar <capital.nwemarwin@gmail.com>
2021-11-09 08:26:25 +0630

    Add project files.

---
+++ DetectBrowser/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DetectBrowser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
